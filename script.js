4b49abce2e86e499e94392a7a56ef3734b6d26c7
Arjun Ghimire <ghimirearjun15@gmail.com>
2020-12-15 15:24:03 -0500

    adding html elemeent and jq dynemic class

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-var body = document.getElementById("body");
-//
+console.log("is app connecting?")
+ //printing date.
+ const m = moment();
+ //hour variable to store the current hour and to check the present past or future class.
+ var hour = moment().hours();
+ document.getElementById('currentDay').innerHTML = m.format("dddd MMM Do h:mm");
+
+pporf();
+function pporf(){
+    $('textarea').each (function(){
+        //grabbing the value of id
+        var textareaValue = $(this).attr('id');
+        var textareahour = parseInt(textareaValue);
+        if (textareahour === hour) {
+            $(this).addClass('present');
+            
+        }
+        else if(textareahour<hour){
+            $(this).addClass('past');
+        }
+        else{
+            $(this).addClass('future');
+        }
+
+    })
+}
